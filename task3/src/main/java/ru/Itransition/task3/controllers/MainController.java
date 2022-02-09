c9ee3e1ad41c1c41dfede8819a74923c0416ecba
Amedomaro <amedomaro13@gmail.com>
2022-02-09 22:42:58 +0300

    Create MainController

--- task3/src/main/java/ru/Itransition/task3/controllers/MainController.java
+++ task3/src/main/java/ru/Itransition/task3/controllers/MainController.java
@@ -1,4 +1,16 @@
 package ru.Itransition.task3.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class MainController {
+
+    @GetMapping("/")
+    public String toGetHomePage(Model model) {
+        model.addAttribute("title", "The main page");
+        return "homePage";
+    }
+
 }
