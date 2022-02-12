9ebd4e9434a03b8aa56e1e03dad790b719698deb
Amedomaro <amedomaro13@gmail.com>
2022-02-12 19:20:14 +0300

    Add UserRepository, RoleRepository

---
+++ task3/src/main/java/ru/Itransition/task3/repository/RoleRepository.java
@@ -0,0 +1,8 @@
+package ru.Itransition.task3.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import ru.Itransition.task3.model.Role;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Role findByName(String name);
+}
---
+++ task3/src/main/java/ru/Itransition/task3/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package ru.Itransition.task3.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import ru.Itransition.task3.model.User;
+
+public interface UserRepository extends JpaRepository <User, Long>{
+    User findByUsername(String name);
+}
