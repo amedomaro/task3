687a0f48ad29c9b375c04de34166823658795a2d
Amedomaro <amedomaro13@gmail.com>
2022-02-15 12:18:53 +0300

    refactor UserAuthService

--- task3/src/main/java/ru/Itransition/task3/service/UserAuthService.java
+++ task3/src/main/java/ru/Itransition/task3/service/UserAuthService.java
@@ -1,6 +1,8 @@
 package ru.Itransition.task3.service;
 
 import java.util.Collections;
+import java.util.Date;
+import java.util.Optional;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,11 +27,18 @@ public class UserAuthService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<ru.Itransition.task3.model.User> myUser = userRepository.findByUsername(username);
+        if (myUser.get().isAccountNonLocked(myUser.get())) {
+            myUser.get().setUpdated(new Date());
+            userRepository.save(myUser.get());
         return userRepository.findByUsername(username)
                         .map(user -> new User(
                         user.getUsername(),
                         user.getPassword(),
                         Collections.singletonList(new SimpleGrantedAuthority("USER"))
                 )).orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        } else {
+            throw new UsernameNotFoundException("User blocked");
+        }
     }
 }
