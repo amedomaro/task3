a157fe7f22820b86ca0c8ba5e13b6d30a7a70ab8
Aleksandr <amedomaro@mail.ru>
2022-02-18 08:38:01 +0300

    add request method userTarget, deleteUser, blockUser, unlockUser
    Signed-off-by: Amedomaro <amedomaro13@gmail.com>

--- task3/src/main/java/ru/Itransition/task3/controllers/UserController.java
+++ task3/src/main/java/ru/Itransition/task3/controllers/UserController.java
@@ -10,112 +10,129 @@ import org.springframework.web.bind.annotation.*;
 import ru.Itransition.task3.model.Status;
 import ru.Itransition.task3.model.User;
 import ru.Itransition.task3.repository.UserRepository;
-
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletException;
+import ru.Itransition.task3.service.UserAuthService;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.util.Arrays;
-import java.util.List;
+
 
 @Controller
 public class UserController {
 
     private User user;
+    private UserAuthService userAuthService;
+    private HttpServletRequest httpServletRequest;
+    private HttpServletResponse httpServletResponse;
 
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    public UserController(UserAuthService userAuthService, HttpServletRequest httpServletRequest,
+                          HttpServletResponse httpServletResponse, UserRepository userRepository) {
+        this.userAuthService = userAuthService;
+        this.httpServletRequest = httpServletRequest;
+        this.httpServletResponse = httpServletResponse;
+        this.userRepository = userRepository;
+    }
+
     @GetMapping("/allUsers")
-    public String showUsers(Model model){
+    public String showUsers(Model model) {
         Iterable<User> user = userRepository.findAll();
         model.addAttribute("user", user);
         return "allUsers";
     }
 
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-
-        String[] names = request.getParameterValues("names");
-        List list = Arrays.asList(names);
-        request.setAttribute("names", list);
-        RequestDispatcher rd = request.getRequestDispatcher("employee.jsp");
-        rd.forward(request, response);
-        for (String str : names){
-            System.out.println(str);
-        }
-    }
-
-//    @GetMapping("/goga")
-//    public String test(@RequestParam("srch-term") String str, Model model){
-//        System.out.println(str);
-//        return "redirect:/allUser";
-//    }
-
-    @GetMapping("/users/delete/{id}")
-    public String delete(@PathVariable(value = "id") Long id){
-        user = userRepository.findById(id).orElseThrow();
-        userRepository.delete(user);
+    @RequestMapping(value = "/target", method = RequestMethod.GET)
+    public String userTarget(HttpServletRequest request) {
+        String[] checkBox = request.getParameterValues("checkBoxUser");
+        String[] delete = request.getParameterValues("delete");
+        String[] block = request.getParameterValues("block");
+        String[] unlock = request.getParameterValues("unlock");
+        if (checkBox == null) return "redirect:/allUsers";
+        if (delete != null) deleteUser(checkBox);
+        if (block != null) blockUser(checkBox);
+        if (unlock != null) unlockUser(checkBox);
         return "redirect:/allUsers";
     }
 
-    @GetMapping("/users/block")
-    public String userBlockedAll(HttpServletRequest request, HttpServletResponse response) {
-        List<User> users = userRepository.findAll();
-        for (User user : users) {
+    private String deleteUser(String[] checkBox) {
+        for (String id : checkBox) {
+            user = userRepository.findById(Long.parseLong(id)).orElseThrow();
+            userRepository.delete(user);
+        }
+        return "redirect:/allUsers";
+    }
+
+    private String blockUser(String[] checkBox) {
+        boolean isFlag = false;
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+
+        for (String id : checkBox) {
+            user = userRepository.findById(Long.parseLong(id)).orElseThrow();
             user.setStatus(Status.BLOCKED);
             userRepository.save(user);
+            if (user.getUsername().equals(auth.getName())) isFlag = true;
         }
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        new SecurityContextLogoutHandler().logout(request, response, auth);
-        return "redirect:/login?logout";
-    }
-
-    @RequestMapping(value = "/users/blocked/{id}", method = RequestMethod.GET)
-    public String logoutPage(HttpServletRequest request,
-                             HttpServletResponse response,
-                             @PathVariable(value = "id") Long id) {
-        user = userRepository.findById(id).orElseThrow();
-        user.setStatus(Status.BLOCKED);
-        userRepository.save(user);
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        if (user.getUsername().equals(auth.getName())) {
-            new SecurityContextLogoutHandler().logout(request, response, auth);
-            return "redirect:/login?logout";
+        if (isFlag){
+            new SecurityContextLogoutHandler().logout(httpServletRequest, httpServletResponse, auth);
+            return "redirect:/login";
         }
         return "redirect:/allUsers";
     }
 
-
-    @GetMapping("/users/blockUnblock/{id}")
-    public String blockUnblock(@PathVariable(value = "id") Long id){
-        user = userRepository.findById(id).orElseThrow();
-        user.setStatus(user.getStatus().equals(Status.BLOCKED)? Status.ACTIVE : Status.BLOCKED);
-        userRepository.save(user);
-//        if(user.getStatus().equals(Status.BLOCKED)){
-//            return "redirect:/login";
-//        }
-        return "redirect:/allUsers";
-    }
-
-    @GetMapping("/users/allBlocked")
-    public String userBlockedAll(){
-        List<User> users =userRepository.findAll();
-        for (User user : users){
-           user.setStatus(Status.BLOCKED);
-            userRepository.save(user);
-        }
-        return "redirect:/allUsers";
-    }
-
-    @GetMapping("/users/allUnblocked")
-    public String userUnBlockedAll(){
-        List<User> users =userRepository.findAll();
-        for (User user : users){
+    private String unlockUser(String[] checkBox) {
+        for (String id : checkBox) {
+            user = userRepository.findById(Long.parseLong(id)).orElseThrow();
             user.setStatus(Status.ACTIVE);
             userRepository.save(user);
         }
         return "redirect:/allUsers";
     }
+
+//    @RequestMapping(value = "/block", method = RequestMethod.GET)
+//    public String userIsActive(HttpServletRequest request,
+//                               HttpServletResponse response) {
+//
+//        String[] check = request.getParameterValues("isChecked");
+//        if (check == null) {
+//            return "redirect:/allUsers";
+//        } else {
+//            for (int i = 0; i < check.length; i++) {
+//                Long id = Long.parseLong(check[i]);
+//                user = userRepository.getById(id);
+//                user.setStatus(Status.BLOCKED);
+//                userRepository.save(user);
+//            }
+//        }
+//        return "redirect:/allUsers";
+//    }
+//
+//    @GetMapping("/users/delete/{id}")
+//    public String delete(@PathVariable(value = "id") Long id) {
+//        user = userRepository.findById(id).orElseThrow();
+//        userRepository.delete(user);
+//        return "redirect:/allUsers";
+//    }
+//
+//    @RequestMapping(value = "/users/blocked/{id}", method = RequestMethod.GET)
+//    public String logoutPage(HttpServletRequest request, HttpServletResponse response,
+//                             @PathVariable(value = "id") Long id) {
+//        user = userRepository.findById(id).orElseThrow();
+//        user.setStatus(Status.BLOCKED);
+//        userRepository.save(user);
+//        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+//        if (user.getUsername().equals(auth.getName())) {
+//            new SecurityContextLogoutHandler().logout(request, response, auth);
+//            return "redirect:/login?logout";
+//        }
+//        return "redirect:/allUsers";
+//    }
+//
+//    @GetMapping("/users/blockUnblock/{id}")
+//    public String blockUnblock(@PathVariable(value = "id") Long id) {
+//        user = userRepository.findById(id).orElseThrow();
+//        user.setStatus(user.getStatus().equals(Status.BLOCKED) ? Status.ACTIVE : Status.BLOCKED);
+//        userRepository.save(user);
+//        return "redirect:/allUsers";
+//    }
 }
